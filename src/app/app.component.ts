0247e2ed4311d76427efeda58cccc193ebff6ef6
none <none>
2024-01-08 17:14:09 -0500

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,152 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+	AuthModule,
+	LoginResponse,
+	OidcSecurityService,
+} from 'angular-auth-oidc-client';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, retry, throwError } from 'rxjs';
+
+export function triggerBrowserFileDownload(fileBlob: Blob, fileName: string) {
+	const fileObjectUrl = URL.createObjectURL(fileBlob);
+	const downloadFileLink = document.createElement('a');
+
+	downloadFileLink.href = fileObjectUrl;
+	downloadFileLink.download = fileName;
+
+	document.body.appendChild(downloadFileLink);
+	downloadFileLink.click();
+	document.body.removeChild(downloadFileLink);
+
+	URL.revokeObjectURL(fileObjectUrl);
+}
+
+type File = {
+	id: number;
+	name: string;
+};
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [CommonModule, RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+	providers: [OidcSecurityService],
 })
-export class AppComponent {
-  title = 'yggdrasil-web';
+@Injectable({ providedIn: 'root' })
+export class AppComponent implements OnInit {
+	title = 'yggdrasil-web';
+	isAuthenticated = false;
+	files: File[] = [];
+	userData: any = null;
+	fileToSubmit: any = null;
+
+	constructor(
+		public oidcSecurityService: OidcSecurityService,
+		private http: HttpClient,
+	) { }
+
+	getFiles(): Observable<File[]> {
+		return this.http
+			.get<File[]>('http://localhost:8080/files')
+			.pipe(retry(1), catchError(this.processError));
+	}
+
+	loadFiles() {
+		this.getFiles().subscribe((result) => {
+			// console.log('>>>> result');
+
+			this.files = result;
+		});
+	}
+
+	processError(err: any) {
+		let message = '';
+		if (err.error instanceof ErrorEvent) {
+			message = err.error.message;
+		} else {
+			message = `Error Code: ${err.status}\nMessage: ${err.message}`;
+		}
+		console.log(message);
+		return throwError(() => {
+			message;
+		});
+	}
+
+	ngOnInit() {
+		this.http.get('http://localhost:8080/files').pipe(
+			retry(1),
+			catchError(() => '!'),
+		);
+
+		this.oidcSecurityService
+			.checkAuth()
+			.subscribe((loginResponse: LoginResponse) => {
+				const { isAuthenticated, userData, accessToken, idToken, configId } =
+					loginResponse;
+
+				this.isAuthenticated = isAuthenticated;
+				this.userData = userData;
+				// console.log('>>> userdata', userData);
+
+				/*...*/
+			});
+	}
+
+	login() {
+		this.oidcSecurityService.authorize();
+	}
+
+	logout() {
+		this.oidcSecurityService
+			.logoff()
+			.subscribe((result) => console.log(result));
+	}
+
+	onFileChange(e: HTMLInputElement) {
+		const file = e.files?.item(0);
+		// console.log(">>> e", e.files?.item(0));
+		this.fileToSubmit = file;
+	}
+
+	onFileSubmit() {
+		const fileRecordData = new FormData();
+		fileRecordData.append('file', this.fileToSubmit);
+
+		this.http
+			.post<File>('http://localhost:8080/files', fileRecordData)
+			.pipe(retry(1), catchError(this.processError))
+			.subscribe((result) => {
+				// console.log('>>> result', result);
+				this.loadFiles();
+			});
+	}
+
+	onDeleteFile(fileID: number) {
+		this.http
+			.delete(`http://localhost:8080/files/${fileID}`)
+			.pipe(retry(1), catchError(this.processError))
+			.subscribe((result) => {
+				// console.log('>>> result', result);
+				this.loadFiles();
+			});
+	}
+
+	onFileDownload(fileID: number, name: string) {
+		this.http
+			.get(`http://localhost:8080/files/${fileID}/download`, {
+				responseType: 'blob',
+			})
+			.subscribe((blobData: Blob) => {
+				triggerBrowserFileDownload(blobData, name)
+			});
+		// .pipe(retry(1), catchError(this.processError))
+		// .subscribe((result) => {
+		// 	console.log('>>> result', result);
+		// 	// triggerBrowserFileDownload(result, name)
+		// });
+	}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,31 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LogLevel, authInterceptor, provideAuth } from 'angular-auth-oidc-client';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+	providers: [
+		provideRouter(routes),
+		provideHttpClient(withInterceptors([authInterceptor()])),
+		provideAuth({
+			config: {
+				authority: 'http://localhost:8180/realms/quickstart',
+				redirectUrl: window.location.origin,
+				postLogoutRedirectUri: window.location.origin,
+				clientId: 'authz-servlet',
+				scope: 'openid profile email',
+				responseType: 'code',
+				silentRenew: true,
+				useRefreshToken: true,
+				logLevel: LogLevel.Debug,
+				customParamsCodeRequest: {
+					client_secret: 'secret',
+				},
+				secureRoutes: [
+					'http://localhost:8080'
+				],
+			},
+		}),
+	],
 };
